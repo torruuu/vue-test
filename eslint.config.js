21261d829fcc34d7a6410ba54502aa3ef9c403e9
Alejandro <ale.torrubiano@gmail.com>
2026-01-20 19:55:26 +0100

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from "@eslint/js"
+import globals from "globals"
+import pluginVue from "eslint-plugin-vue"
+import json from "@eslint/json"
+import { defineConfig } from "eslint/config"
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'no-unused-vars': ["warn"],
+      'arrow-spacing': "error",
+    }
+  },
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+])
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Header from './components/Header.vue';
+import Button from './components/Button.vue';
+
+const test = () => {
+  console.log("test4")
+}
+
+const test2 = 2
+</script>
+
+<template>
+  <Header />
+  <Button />
+</template>
+
+<style scoped></style>
