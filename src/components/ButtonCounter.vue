ad43d52ba70b9494b64dc399afb552bed6046887
Alejandro <ale.torrubiano@gmail.com>
2026-01-21 17:34:29 +0100

    feat: button

---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+let count = ref(0)
+
+const handleClick = () => {
+  console.log('Button clicked');
+  count.value = count.value + 1;
+}
+</script>
+
+<template>
+  <div class="text-2xl font-bold">{{ count }}</div>
+  <button class="bg-gray-500 text-white p-2 rounded-md cursor-pointer" @click="handleClick">
+    Click ++
+  </button>
+</template>
