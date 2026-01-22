c1e76b7907772f0f19885df006b620216f750285
Alejandro <ale.torrubiano@gmail.com>
2026-01-22 16:23:23 +0100

    fix: add

--- src/components/ButtonCounter.vue
+++ src/components/ButtonCounter.vue
@@ -1,21 +1,43 @@
 <script setup>
 import { ref } from 'vue';
 
-/**
- * test comment
- */
-
 let count = ref(0)
+const inputText = ref('')
 
-const handleClick = () => {
-  console.log('Button clicked');
-  count.value = count.value + 1;
+const handleClick = (event) => {
+  event.stopPropagation()
+  count.value++;
 }
+const handleClick2 = () => {
+  count.value--;
+}
+const handleInput = (event) => {
+  inputText.value = event.target.value
+}
+
 </script>
 
 <template>
   <div class="text-2xl font-bold">{{ count }}</div>
-  <button class="bg-gray-500 text-white p-2 rounded-md cursor-pointer" @click="handleClick">
-    Click ++
-  </button>
+  <span v-if="count >= 5">Has llegado a 5</span>
+  <span v-else-if="count === 3">Estas en 3</span>
+  <br>
+  <div @click="console.log('click desde el padre')">
+    <button class="bg-gray-500 text-white p-2 rounded-md cursor-pointer" @click.stop="handleClick">
+      Click ++
+    </button>
+    <button class="bg-gray-500 text-white p-2 rounded-md cursor-pointer" @click="handleClick2">
+      Click --
+    </button>
+  </div>
+  <br>
+  <span >{{ inputText }}</span>
+  <br>
+
+  <form @submit.prevent="console.log('He hecho un submit')">
+    <input @input="handleInput" class="bg-gray-200" type="text">
+    <button class="bg-gray-500 text-white p-2 rounded-md cursor-pointer">
+      Enviar
+    </button>
+  </form>
 </template>
