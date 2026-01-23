b9ed91e54cb015d2591c51a68a3560d5695e0e56
Alejandro <ale.torrubiano@gmail.com>
2026-01-23 19:54:22 +0100

    chore: ejercicio1

---
+++ src/components/Ejercicio1/FormHijo.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+defineProps({
+  input: String
+})
+
+
+
+</script>
+
+<template>
+  <div id="22" class="aa" >
+    
+    <span>
+      {{ input }} 
+    </span>
+
+    <button>Borrar</button>  
+  </div>
+  
+
+</template>
---
+++ src/components/Ejercicio1/FormPadre.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+  import FormHijo from "./FormHijo.vue"
+  import { ref } from "vue";
+  
+  const inputText= ref('');
+
+  function FormHijoo({ input }) {
+    console.log(input)
+  }
+
+  FormHijoo({ input: 'aaa' })
+
+
+</script>
+
+<template>
+
+  <div>
+    <input v-model="inputText" class="border-2 bg-amber-300">
+    
+  </div>
+
+  <FormHijo :input="inputText" />
+
+
+</template>
