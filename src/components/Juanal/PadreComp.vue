d79a2abbaf188428bc8823fd3be106e4f5399baa
JuanaL7 <juanalmart7@gmail.com>
2026-01-30 16:27:30 +0100

    chore: madre...

---
+++ src/components/Juanal/PadreComp.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+import HijoComp from './HijoComp.vue';
+
+const tareas = [
+    { id: 1, title: "Comer", estado: true},
+    { id: 2, title: "Dormir", estado: false}]
+
+const tareasPendientes = [];
+const tareasTerminadas = [];
+
+
+ function addTarea () {
+        if(tareas.estado===true) {
+            tareasPendientes.push
+        }
+        else {tareasTerminadas.push}
+    }
+
+    
+
+
+
+
+
+
+</script>
+<template>
+    <div>
+    <HijoComp :tareasPendientes="tareasPendientes" :tareasTerminadas="tareasTerminadas"/>
+        
+    
+
+
+
+    </div>
+
+
+
+</template>
