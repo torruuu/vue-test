df0830f3afbd6180b692b609a91ce9de26826c66
Alejandro <ale.torrubiano@gmail.com>
2026-02-03 18:35:25 +0100

    chore: falta el input buscar

--- src/components/Laura/TareaPadre.vue
+++ src/components/Laura/TareaPadre.vue
@@ -2,13 +2,37 @@
 import TareaHijo from "./TareaHijo.vue"
 import TareaHija from "./TareaHija.vue"
 import TareaBuscador from "./TareaBuscador.vue"
-import { ref } from "vue"
+import { computed, ref } from "vue"
 
 const tareas = ref([
     { id: 1, nombre: "Lavar los platos", estado: false },
     { id: 2, nombre: "Terminar los ejercicios de Vue", estado: false },
     { id: 3, nombre: "Hacer la compra", estado: false },
 ])
+
+const tareasTerminadas = computed(recuperarCompl);
+const tareasPendientes = computed(recuperarPend);
+
+
+function recuperarPend (){
+    const tareasPend = [];
+    for (let tarea of tareas.value){
+        if(!tarea.estado){
+            tareasPend.push(tarea)
+        }
+    }
+    return tareasPend
+}
+
+function recuperarCompl (){
+    const tareasCompl = [];
+    for (let tarea of tareas.value){
+        if(tarea.estado){
+            tareasCompl.push(tarea)
+        }
+    }
+    return tareasCompl
+}
 
 //Declaro la funcion borrar tarea
 function eliminarTarea(id) {
@@ -17,7 +41,7 @@ function eliminarTarea(id) {
 //Declaro la funcion agregar tarea que añade el valor del input en el array
 function agregarTarea(nombreTarea) {
     tareas.value.push({
-        id: tareas.value.length + 1,
+        id: Date.now(),
         nombre: nombreTarea,
         estado: false
     })
@@ -45,13 +69,13 @@ function marcar(id) {
         </ul>
         <TareaHija @agregar-tarea="agregarTarea" />
         <h1 class="mb-20 mt-6 text-xl underline">Lista de tareas:</h1>
-        <ul v-for="tarea in tareas" :key="tarea.id">
-            <TareaHijo v-if="!tarea.estado" :id="tarea.id" :nombre="tarea.nombre" :estado="tarea.estado"
+        <ul v-for="tarea in tareasPendientes" :key="tarea.id">
+            <TareaHijo :id="tarea.id" :nombre="tarea.nombre" :estado="tarea.estado"
                 @marcar="marcar" @borrado="eliminarTarea" />
         </ul>
         <h2 class="mt-6 text-xl underline">Tareas completadas:</h2>
-        <ul v-for="tarea in tareas" :key="tarea.id">
-            <TareaHijo v-if="tarea.estado" :id="tarea.id" :nombre="tarea.nombre" :estado="tarea.estado"
+        <ul v-for="tarea in tareasTerminadas" :key="tarea.id">
+            <TareaHijo :id="tarea.id" :nombre="tarea.nombre" :estado="tarea.estado"
                 @borrado="eliminarTarea" />
         </ul>
     </div>
