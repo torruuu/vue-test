bfa4856668ff96ef7c4f4645d33e3489530999d0
ATKLA <ateca.vega@gmail.com>
2026-02-04 13:09:44 +0100

    chore: hasta donde he podido

--- src/components/Laura/TareaBuscador.vue
+++ src/components/Laura/TareaBuscador.vue
@@ -1,20 +1,15 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref } from 'vue';
 
-const props = defineProps({
-    tareas: Array
-})
+
 
 const tareaBuscada = ref("");
+const emit = defineEmits(['buscar-tarea'])
 
-const filtradoComputed = computed(() => {
-    //const tareaEncontrada = [""];
-    if (props.value.includes(tareaBuscada.value)) {
-        //tareaEncontrada.push(props);
-        return props.tareas.value;
-    }
-    return "";
-})
+function buscarTarea() {
+    emit('buscar-tarea', tareaBuscada.value)
+}
+
 
 
 </script>
@@ -23,9 +18,9 @@ const filtradoComputed = computed(() => {
     <div class="flex flex-col items-center">
         <h1 class="text-3xl underline">To do list.</h1>
         <li>
-            <input v-model="tareaBuscada" id="buscador" type="select"
+            <input v-model="tareaBuscada" @input="buscarTarea" id="buscador" type="select"
                 class="bg-gray-200 border pl-2 p-2 mt-4 rounded-l-lg" placeholder="Busca una tarea.">
-            <button @click="filtradoComputed" class="bg-gray-400 p-2 px-4 border rounded-r-lg">🔍</button>
+            <button @click="filtradoComputed" class="bg-gray-400 p-2 px-5 border rounded-r-lg">🔍</button>
         </li>
         <span></span>
     </div>
--- src/components/Laura/TareaPadre.vue
+++ src/components/Laura/TareaPadre.vue
@@ -9,25 +9,39 @@ const tareas = ref([
     { id: 2, nombre: "Terminar los ejercicios de Vue", estado: false },
     { id: 3, nombre: "Hacer la compra", estado: false },
 ])
-
+let tareaCoincide = false;
 const tareasTerminadas = computed(recuperarCompl);
 const tareasPendientes = computed(recuperarPend);
+const tareasCoincidenComputed = computed((tareaBuscada) => {
+    const tareasCo = [];
+    for (let tarea of tareas.value) {
+        if (tarea.nombre.includes(tareaBuscada)) {
+            tareasCo.push(tarea.nombre)
+            tareaCoincide = true;
+        }
+    } return tareasCo
+});
 
+function tareafiltrada() {
+    if (tareaCoincide) {
+        return tareasCoincidenComputed
+    }
+}
 
-function recuperarPend (){
+function recuperarPend() {
     const tareasPend = [];
-    for (let tarea of tareas.value){
-        if(!tarea.estado){
+    for (let tarea of tareas.value) {
+        if (!tarea.estado) {
             tareasPend.push(tarea)
         }
     }
     return tareasPend
 }
 
-function recuperarCompl (){
+function recuperarCompl() {
     const tareasCompl = [];
-    for (let tarea of tareas.value){
-        if(tarea.estado){
+    for (let tarea of tareas.value) {
+        if (tarea.estado) {
             tareasCompl.push(tarea)
         }
     }
@@ -65,18 +79,20 @@ function marcar(id) {
 <template>
     <div class="flex flex-col items-center gap-4 mt-22">
         <ul>
-            <TareaBuscador />
+            <TareaBuscador @buscar-tarea="tareafiltrada" />
         </ul>
-        <TareaHija @agregar-tarea="agregarTarea" />
-        <h1 class="mb-20 mt-6 text-xl underline">Lista de tareas:</h1>
-        <ul v-for="tarea in tareasPendientes" :key="tarea.id">
-            <TareaHijo :id="tarea.id" :nombre="tarea.nombre" :estado="tarea.estado"
-                @marcar="marcar" @borrado="eliminarTarea" />
-        </ul>
-        <h2 class="mt-6 text-xl underline">Tareas completadas:</h2>
-        <ul v-for="tarea in tareasTerminadas" :key="tarea.id">
-            <TareaHijo :id="tarea.id" :nombre="tarea.nombre" :estado="tarea.estado"
-                @borrado="eliminarTarea" />
-        </ul>
+        <span v-if="tareaCoincide">{{ tareasCoincidenComputed }}</span>
+        <div v-if="!tareaCoincide">
+            <TareaHija @agregar-tarea="agregarTarea" />
+            <h1 class="mb-20 mt-6 text-xl underline">Lista de tareas:</h1>
+            <ul v-for="tarea in tareasPendientes" :key="tarea.id">
+                <TareaHijo :id="tarea.id" :nombre="tarea.nombre" :estado="tarea.estado" @marcar="marcar"
+                    @borrado="eliminarTarea" />
+            </ul>
+            <h2 class="mt-6 text-xl underline">Tareas completadas:</h2>
+            <ul v-for="tarea in tareasTerminadas" :key="tarea.id">
+                <TareaHijo :id="tarea.id" :nombre="tarea.nombre" :estado="tarea.estado" @borrado="eliminarTarea" />
+            </ul>
+        </div>
     </div>
 </template>
