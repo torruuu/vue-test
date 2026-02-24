d431923868d2598e0f8b5b08441d611acc81cba7
ATKLA <ateca.vega@gmail.com>
2026-02-24 15:55:31 +0100

    chore: mix color-exercise

--- src/components/Laura/Ej_color/HijoView.vue
+++ src/components/Laura/Ej_color/HijoView.vue
@@ -1,15 +1,33 @@
 <script setup>
+defineProps({
+    color: String,
+    color2: String
+});
 
-const emit = defineEmits(['color-change']);
+const emit = defineEmits(['color-change', 'color-change2']);
+
 
 function changeColor(event) {
     const color = event.target.value;
     emit('color-change', color);
 }
+
+function changeColor2(event) {
+    const color = event.target.value;
+    emit('color-change2', color);
+}
+
 </script>
 
 <template>
-    <div class="flex flex-col  items-center mt-4">
-        <input type="color" @input="changeColor">
+    <div class="flex items-center gap-6 mt-4">
+        <div class="flex flex-col items-center justify-center">
+            <input type="color" @input="changeColor" :value="color">
+            {{ color }}
+        </div>
+        <div class="flex flex-col items-center justify-center">
+            <input type="color" @input="changeColor2" :value="color2">
+            {{ color2 }}
+        </div>
     </div>
 </template>
